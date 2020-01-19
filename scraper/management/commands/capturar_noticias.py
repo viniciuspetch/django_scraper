0a7a377b9752be50cb0844632b8d7edefe82b5ab
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-01-18 23:24:25 -0300

    Added article content

--- scraper/management/commands/capturar_noticias.py
+++ scraper/management/commands/capturar_noticias.py
@@ -15,22 +15,34 @@ def scrape():
     article_list = soup.find_all(class_='tec--carousel__item__title__link')
 
     # For each article, get the ID, URL, title and content
-    for article in article_list:
-        article_title = article.string
-        article_url = article.get('href')
-        article_id = article_url.split('/')[4].split('-')[0]
-        response = requests.get(article_url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        article_content = str(soup.find(class_="tec--article__body"))
+    article = article_list[0]
+    article_title = article.string
+    article_url = article.get('href')
+    article_id = article_url.split('/')[4].split('-')[0]
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    article_content_bs4 = soup.find(class_="tec--article__body")
+    article_content = ""
 
-        # If the ID is not found, it's considered a new article, so it's added to the DB
-        if len(Article.objects.filter(article_id=article_id)) == 0:
-            Article(source='Tecmundo', article_id=article_id, url=article_url,
-                    title=article_title, content=article_content).save()
-            print('!!NEW!! ('+article_id+') '+article_title)
-        # Otherwise, only show on the console
-        else:
-            print('('+article_id+') '+article_title)
+    for tag in article_content_bs4.children:
+        print(tag.name)
+        if tag.name == "p" or tag.name == "h2":
+            article_content = article_content + str(tag)
+        if tag.name == "figure":
+            img = tag.img
+            print(img['data-src'].split('?')[0])
+            print(type(img))
+            article_content = article_content + '<img src="' + img['data-src'].split('?')[0] + '">' + str(tag.figcaption)
+
+
+    # If the ID is not found, it's considered a new article, so it's added to the DB
+    if len(Article.objects.filter(article_id=article_id)) == 0:
+        Article(source='Tecmundo', article_id=article_id, url=article_url,
+                title=article_title, content=article_content).save()
+        print('!!NEW!! ('+article_id+') '+article_title)
+    # Otherwise, only show on the console
+    else:
+        print('('+article_id+') '+article_title)
 
 
 class Command(BaseCommand):
