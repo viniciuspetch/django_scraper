4728db30d9c46ebb1e9b697b8a2400c3d2518fd6
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-01-19 21:43:19 -0300

    News hub routing is now the index page

--- django_scraper/urls.py
+++ django_scraper/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('scraper/', include('scraper.urls')),
+    path('', include('scraper.urls')),
 ]
